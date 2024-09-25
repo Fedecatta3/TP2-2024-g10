b110df5931a7b46a9b0c356020529149f3208bb6
Fedecatta3 <130402198+Fedecatta3@users.noreply.github.com>
2024-09-25 00:46:42 -0300

    Add files via upload

---
+++ CD_usuario.cs
@@ -0,0 +1,152 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+using System.Data.SqlClient;
+using CapaEntidad;
+
+namespace CapaDatos
+{
+    public class CD_usuario
+    {
+        public List<Usuario> Listar(){
+            List<Usuario> lista = new List<Usuario>();
+
+            using (SqlConnection conexion = new SqlConnection(Conexion.cadena)) {
+                try
+                {
+                    //string query = "select id_usuario,id_rol,nombre,email,telefono,dni,fecha_nacimiento,estado, contraseña, apellido FROM usuario ";
+
+                    string query = "SELECT u.id_usuario, u.id_rol, r.descripcion AS rol_descripcion, u.nombre, u.email, u.telefono, u.dni, u.fecha_nacimiento, u.estado, u.contraseña, u.apellido " +
+                               "FROM Usuario u " +
+                               "JOIN Rol r ON u.id_rol = r.id_rol";
+                    SqlCommand cmd = new SqlCommand(query, conexion);
+                    cmd.CommandType = CommandType.Text;
+
+                    conexion.Open();
+
+                    using (SqlDataReader dr = cmd.ExecuteReader())
+                    {
+                        while (dr.Read())
+                        {
+                            lista.Add(new Usuario()
+                            {
+                                id_usuario = Convert.ToInt32(dr["id_usuario"]),
+                                //id_rol = new Rol { id_rol = Convert.ToInt32(dr["id_rol"]) },
+                                id_rol = new Rol { id_rol = Convert.ToInt32(dr["id_rol"]),
+                                                   descripcion = dr["rol_descripcion"].ToString()},
+                                nombre = dr["nombre"].ToString(),
+                                email = dr["email"].ToString(),
+                                telefono = dr["telefono"].ToString(),
+                                dni = dr["dni"].ToString(),
+                                fecha_nacimiento = dr["fecha_nacimiento"].ToString(),
+                                estado = Convert.ToBoolean(dr["estado"]),
+                                contraseña = dr["contraseña"].ToString(),
+                                apellido = dr["apellido"].ToString()
+
+
+                            });
+                        }
+                    }
+                }
+                catch (Exception ex) {
+                    Console.WriteLine("Error: " + ex.Message);
+                    //lista = new List<Usuario>();
+                }
+            }
+            return lista;
+        }
+
+
+        public int Registrar(Usuario obj, out string mensaje)
+        {
+            int idUsuarioGenerado = 0;
+            mensaje = string.Empty;
+
+            try
+            {
+                using (SqlConnection conexion = new SqlConnection(Conexion.cadena))
+                {
+                    SqlCommand cmd = new SqlCommand("SP_REGISTRARUSUARIO", conexion);
+                    cmd.Parameters.AddWithValue("id_rol", obj.id_rol.id_rol);
+                    cmd.Parameters.AddWithValue("nombre", obj.nombre);
+                    cmd.Parameters.AddWithValue("email", obj.email);
+                    cmd.Parameters.AddWithValue("telefono", obj.telefono);
+                    cmd.Parameters.AddWithValue("dni", obj.dni);
+                    cmd.Parameters.AddWithValue("fecha_nacimiento", obj.fecha_nacimiento);
+                    cmd.Parameters.AddWithValue("estado", obj.estado);
+                    cmd.Parameters.AddWithValue("contraseña", obj.contraseña);
+                    cmd.Parameters.AddWithValue("apellido", obj.apellido);
+
+                    //parametros de salida
+                    cmd.Parameters.Add("idUsuarioResultado", SqlDbType.Int).Direction = ParameterDirection.Output;
+                    cmd.Parameters.Add("mensaje", SqlDbType.VarChar, 500).Direction = ParameterDirection.Output;
+
+                    cmd.CommandType = CommandType.StoredProcedure;
+
+                    conexion.Open();
+
+                    cmd.ExecuteNonQuery();
+
+                    idUsuarioGenerado = Convert.ToInt32(cmd.Parameters["idUsuarioResultado"].Value);
+                    mensaje = cmd.Parameters["mensaje"].Value.ToString();
+                }
+            }
+            catch (Exception ex)
+            {
+                idUsuarioGenerado = 0;
+                mensaje = ex.Message;
+            }
+
+            return idUsuarioGenerado;
+        }
+
+
+
+        public bool Editar(Usuario obj, out string mensaje)
+        {
+            bool respuesta = false;
+            mensaje = string.Empty;
+
+            try
+            {
+                using (SqlConnection conexion = new SqlConnection(Conexion.cadena))
+                {
+                    SqlCommand cmd = new SqlCommand("SP_EDITARUSUARIO", conexion);
+                    cmd.Parameters.AddWithValue("id_usuario", obj.id_usuario);
+                    cmd.Parameters.AddWithValue("id_rol", obj.id_rol);
+                    cmd.Parameters.AddWithValue("nombre", obj.nombre);
+                    cmd.Parameters.AddWithValue("email", obj.email);
+                    cmd.Parameters.AddWithValue("telefono", obj.telefono);
+                    cmd.Parameters.AddWithValue("dni", obj.dni);
+                    cmd.Parameters.AddWithValue("fecha_nacimiento", obj.fecha_nacimiento);
+                    cmd.Parameters.AddWithValue("estado", obj.estado);
+                    cmd.Parameters.AddWithValue("contraseña", obj.contraseña);
+                    cmd.Parameters.AddWithValue("apellido", obj.apellido);
+
+                    //parametros de salida
+                    cmd.Parameters.Add("respuesta", SqlDbType.Int).Direction = ParameterDirection.Output;
+                    cmd.Parameters.Add("mensaje", SqlDbType.VarChar, 500).Direction = ParameterDirection.Output;
+
+                    cmd.CommandType = CommandType.StoredProcedure;
+
+                    conexion.Open();
+
+                    cmd.ExecuteNonQuery();
+
+                    respuesta = Convert.ToBoolean(cmd.Parameters["respuesta"].Value);
+                    mensaje = cmd.Parameters["mensaje"].Value.ToString();
+                }
+            }
+            catch (Exception ex)
+            {
+                respuesta = false;
+                mensaje = ex.Message;
+            }
+
+            return respuesta;
+        }
+    }
+}
